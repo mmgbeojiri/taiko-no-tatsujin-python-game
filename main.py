b9d7f9724bb70e43a90603a3f41ed308bbedccae
mmgbeojiri <mmgbeojirikwe4960@baysidehighschool.org>
2024-03-04 12:32:26 -0500

    positioned the outerbar inner and score

--- main.py
+++ main.py
@@ -2,12 +2,27 @@ from gamelib import *
 
 game = Game(1920, 1080, "Taiko no Tatsujin - Python")
 
-outerBar = Image("./images/outerbar.png")
-innerBar = Image("./images/innerbar.png")
-drum = Image("./images/taikodrum.png")
-drumCollide = Image("./images/drumhit.png")
-blue = Image("./images/blue.png")
-red = Image("./images/red.png")
-effect = Image("./images/effect.png")
-scoreContain = Image("./images/scorecontainer.png")
+outerBar = Image("./images/outerbar.png", game)
+innerBar = Image("./images/innerbar.png", game)
 
+outerBar.resizeBy(100)
+innerBar.resizeBy(100)
+scoreContain = Image("./images/scorecontainer.png", game)
+
+scoreContain.resizeBy(-57)
+scoreContain.moveTo(scoreContain.width/2, 153)
+
+drum = Image("./images/taikodrum.png", game)
+drumCollide = Image("./images/drumhit.png", game)
+blue = Image("./images/blue.png", game)
+red = Image("./images/red.png", game)
+effect = Image("./images/effect.png", game)
+
+while not game.over:
+    game.processInput()
+    outerBar.draw()
+    innerBar.draw()
+
+    scoreContain.draw()
+    game.update(60)
+game.quit()
