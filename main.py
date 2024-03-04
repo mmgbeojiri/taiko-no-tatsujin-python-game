483ffd5ec9f903e708d5d039fb387e0ec87b5c1f
mmgbeojiri <mmgbeojirikwe4960@baysidehighschool.org>
2024-03-04 12:18:40 -0500

    initialized varabiles

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from gamelib import *
+
+game = Game(1920, 1080, "Taiko no Tatsujin - Python")
+
+outerBar = Image("./images/outerbar.png")
+innerBar = Image("./images/innerbar.png")
+drum = Image("./images/taikodrum.png")
+drumCollide = Image("./images/drumhit.png")
+blue = Image("./images/blue.png")
+red = Image("./images/red.png")
+effect = Image("./images/effect.png")
+scoreContain = Image("./images/scorecontainer.png")
 
