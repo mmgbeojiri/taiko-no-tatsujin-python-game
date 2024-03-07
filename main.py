6c10ab54c310c2acccc867995ab58a6a39eea383
mmgbeojiri <mmgbeojirikwe4960@baysidehighschool.org>
2024-03-07 12:01:27 -0500

    added sounds

--- main.py
+++ main.py
@@ -26,19 +26,25 @@ innerBar = Image("./images/innerbar.png", game)
 
 outerBar.resizeBy(100)
 innerBar.resizeBy(100)
+
+yPositionLine = 153
+
 scoreContain = Image("./images/scorecontainer.png", game)
 
 scoreContain.resizeBy(-57)
-scoreContain.moveTo(scoreContain.width/2, 153)
+scoreContain.moveTo(scoreContain.width/2, yPositionLine)
 
 
 drum = Image("./images/taikodrum.png", game)
 drumCollide = Image("./images/drumhit.png", game)
 
 drum.resizeBy(-78)
+drumCollide.resizeBy(-78)
 
 
-drum.moveTo(scoreContain.width - 100, 153)
+drum.moveTo(scoreContain.width - 100, yPositionLine)
+drumCollide.moveTo(drum.x + 200, yPositionLine)
+beatLine = drumCollide.x
 
 drumInnerRight = Image("./images/drum/innerright.png", game)
 drumInnerLeft = Image("./images/drum/innerleft.png", game)
@@ -114,12 +120,19 @@ innerRightKeys = [
     K_COMMA
 ]
 
+# Initialize debounce flags for each key
+debounce_flags = {
+    key: False for key in outerLeftKeys + outerRightKeys + innerLeftKeys + innerRightKeys
+}
+
 while not game.over:
     game.processInput()
     game.clearBackground()
     outerBar.draw()
     innerBar.draw()
+    drumCollide.draw()
 
+    # Notes #
 
     scoreContain.draw()
     drum.draw()
@@ -137,8 +150,21 @@ while not game.over:
     for i in range(len(innerRightKeys)): # Don
         if keys.Pressed[innerRightKeys[i]]:
             drumInnerRight.draw()
-
+    
     # for each key, make a sound debounce. when key is pressed, play sound only once. 
+
+    # Inside the game loop, handle key press events
+    for key in debounce_flags:
+        if keys.Pressed[key] and not debounce_flags[key]:  # Check if key is pressed and debounce flag is False
+            debounce_flags[key] = True  # Set debounce flag to True to prevent multiple plays
+            # Play corresponding sound based on key
+            if key in innerLeftKeys or key in innerRightKeys:  # Don sound
+                don.play()
+            else:  # Katsu sound
+                katsu.play()
+        elif not keys.Pressed[key] and debounce_flags[key]:  # Check if key is released and debounce flag is True
+            debounce_flags[key] = False  # Reset debounce flag
+
 
 
     game.update(60)
