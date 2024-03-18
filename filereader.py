7dc44e5551b1dcef3456eb10299fffcceac1116d
mmgbeojiri <mmgbeojirikwe4960@baysidehighschool.org>
2024-03-18 11:55:57 -0400

    IM TRYING TO DO WORK IM SORRY FOR LACK OF UDPATE

--- filereader.py
+++ filereader.py
@@ -2,6 +2,7 @@ bpm = 120
 measure = 0
 noteIndex = 0
 lengthOfMeasure = 5
-measureDuration = 1/bpm/60
-noteTimeStamp = measureDuration * measure
-noteTimeStamp += (noteIndex/lengthOfMeasure * measureDuration)
+def findNextNote():
+    measureDuration = 1/bpm/60
+    noteTimeStamp = measureDuration * measure
+    noteTimeStamp += (noteIndex/lengthOfMeasure * measureDuration)
